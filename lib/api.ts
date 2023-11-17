856a8b8e475939c8b47ad01cfa6d4de20232e69e
Sai Htet Wai Yan <saihtetwaiyan18@gmail.com>
2023-11-17 11:31:25 +0630

    Remove Some Pages and fix api

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,11 @@
 "use server";
 export async function latestData() {
-  const res = await fetch("https://forex.cbm.gov.mm/api/latest");
+  const res = await fetch("https://forex.cbm.gov.mm/api/latest", {
+    headers: {
+      "Cache-Control": "no-cache",
+      Pragma: "no-cache",
+    },
+  });
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
@@ -8,7 +13,12 @@ export async function latestData() {
 }
 
 export async function historyData(date: Date) {
-  const res = await fetch(`http://forex.cbm.gov.mm/api/history/${date}`);
+  const res = await fetch(`http://forex.cbm.gov.mm/api/history/${date}`, {
+    headers: {
+      "Cache-Control": "no-cache",
+      Pragma: "no-cache",
+    },
+  });
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
